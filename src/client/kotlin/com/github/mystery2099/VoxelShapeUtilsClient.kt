5f3c13cca765b83578e5d6461868d086d1684ce3
Mathew Kennedy-Brewer <xxmysteryxxftb@gmail.com>
2023-11-29 16:40:34 -0500

    Added ability to be published to github packages, and published it

--- src/client/kotlin/com/github/mystery2099/VoxelShapeUtilsClient.kt
+++
@@ -1,9 +0,0 @@
-package com.github.mystery2099
-
-import net.fabricmc.api.ClientModInitializer
-
-object VoxelShapeUtilsClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
