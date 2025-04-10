994fc44f9d80a40dc752d9d91fd42321a40c7880
Mystery2099 <xxmysteryxxftb@gmail.com>
2025-04-10 11:10:36 -0400

    chore: bump version to 1.2.0 and update metadata
    - Update version in gradle.properties to 1.2.0+1.19.4
    - Add version constants to main VoxLib class
    - Improve initialization logging
    - Update documentation references
    
    Signed-off-by: Mystery2099 <xxmysteryxxftb@gmail.com>

--- src/main/kotlin/com/github/mystery2099/voxlib/VoxLib.kt
+++ src/main/kotlin/com/github/mystery2099/voxlib/VoxLib.kt
@@ -3,10 +3,25 @@ package com.github.mystery2099.voxlib
 import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
+/**
+ * Main entry point for the VoxLib mod.
+ * This library provides utilities for manipulating, creating, and rotating voxel shapes.
+ */
 object VoxLib : ModInitializer {
     private val logger = LoggerFactory.getLogger("voxlib")
 
+    /**
+     * The current version of VoxLib.
+     */
+    private const val VERSION = "1.2.0"
+
+    /**
+     * The Minecraft version this mod is built for.
+     */
+    private const val MINECRAFT_VERSION = "1.19.4"
+
 	override fun onInitialize() {
-		logger.info("Hello Fabric world, this is VoxLib!")
+        logger.info("Initializing VoxLib v$VERSION for Minecraft $MINECRAFT_VERSION")
+        logger.info("VoxLib is ready to help with your voxel shape needs!")
 	}
 }
